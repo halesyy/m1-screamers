df316787deb50a0b2fbaf4548742d17a3b8b5ca0
Jack Hales <halesyy@gmail.com>
2021-03-04 00:07:24 +1100

    bruh

---
+++ screamers/2-threaded-scraping.py
@@ -0,0 +1,39 @@
+
+"""
+A simple scrape-and-BSOUP parse on my own website.
+Angles: compute, single-core threading.
+Works: Python 3.
+"""
+
+from timeit import default_timer as timer
+from concurrent.futures import ThreadPoolExecutor
+
+"""
+Make request to jackhales.com, print out response + page len
+and use BeautifulSoup to compute into a class.
+"""
+def just_a_request(id):
+    from requests import get
+    from bs4 import BeautifulSoup
+    page = get("https://jackhales.com")
+    soup = BeautifulSoup(page.text, "html.parser")
+    print("finished id:", id, "with page len:", len(page.text))
+    return True
+
+if __name__ == "__main__":
+
+    start = timer()
+
+    with ThreadPoolExecutor(500) as handler:
+        results = handler.map(just_a_request, range(500))
+
+    end = timer()
+
+    if True: # set to false if you dont want extra compute
+        worked = len([True for t in results if t == True])
+        not_worked = len([False for t in results if t == None]) # t == None if excepts in-thread
+        print("out of all req,", worked, "worked and", not_worked, "didn't")
+
+    time_taken = (end - start)
+
+    print("seconded {}".format(time_taken))
